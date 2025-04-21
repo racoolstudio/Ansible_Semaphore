b29538626393727297c0391c1f634a740585b19b
Ridwan Abdulwaheed <75172422+racoolstudio@users.noreply.github.com>
2025-04-21 12:27:47 -0230

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /var/www/html
+mkdir temp && cd temp
+wget https://www.tooplate.com/zip-templates/2130_waso_strategy.zip 
+unzip -o 2130_waso_strategy.zip
+cd 2130_waso_strategy
+cp -r * ../../
+sudo systemctl restart apache2 
